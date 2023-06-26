c395f4c64db977011145849843b7017cab4bc566
ally4 <el.ally741@gmail.com>
2023-06-26 14:18:52 +0200

    welcome message

--- src/controllers/welcome.js
+++ src/controllers/welcome.js
@@ -3,7 +3,7 @@ import app from '../languages/config';
 const welcome = app.get('/', (req, res) => {
   res.status(200).json({
     status: res.__(200),
-    message: res.__('Welcome to phantom, a platform to facilitate the transportation mode in town!.'),
+    message: res.__('Welcome to acubed, a platform to facilitate the transportation your samples for a great, faster and right testing!.'),
   });
 });
 
